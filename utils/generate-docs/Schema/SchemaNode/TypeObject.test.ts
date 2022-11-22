721216b41b5094d87155b67a4910d443af2f44e9
John Scrudato <jscrudato@gunder.com>
2022-11-22 11:41:10 -0500

    Add MkDocs test case for TypeObject.

--- utils/generate-docs/Schema/SchemaNode/TypeObject.test.ts
+++ utils/generate-docs/Schema/SchemaNode/TypeObject.test.ts
@@ -62,4 +62,51 @@ Copyright © 2022 Open Cap Table Coalition.
 `);
     });
   });
+
+  describe("#markdownOutputMkDocs", () => {
+    it("returns a string representing the node as Markdown for display on MkDocs", () => {
+      const schema = new Schema(
+        [FIXTURE],
+        [],
+        [],
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      );
+      const actual = new TypeObject(
+        schema,
+        FIXTURE,
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      ).markdownOutput();
+
+      expect(actual)
+        .toEqual(`:house: [Documentation Home](https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README)
+
+---
+
+### Test Title
+
+\`https://opencaptablecoalition.com/schema/types/TestTypeObject.schema.json\`
+
+_This is a test fixture exemplifying an TypeObject schema from OCF_
+
+**Data Type:** \`OCF TYPE\`
+
+**Properties:**
+
+| Property     | Type                                                                                                                           | Description                                                       | Required   |
+| ------------ | ------------------------------------------------------------------------------------------------------------------------------ | ----------------------------------------------------------------- | ---------- |
+| 1            | \`STRING\`                                                                                                                       | Example description                                               | \`REQUIRED\` |
+| refProperty1 | [schema/types/TestTypeObject](https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs/schema/types/TestTypeObject) | This is a test fixture exemplifying an TypeObject schema from OCF | -          |
+
+**Source Code:** [schema/types/TestTypeObject](https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF/schema/types/TestTypeObject.schema.json)
+
+Copyright © 2022 Open Cap Table Coalition.
+`);
+    });
+  });
 });
