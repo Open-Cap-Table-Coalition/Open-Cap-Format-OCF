bf9589478bac3e631fe632f83c51915070b58d16
John Scrudato <jscrudato@gunder.com>
2022-11-22 11:36:11 -0500

    Added MkDocs test cases for SchemaNode path tests.

--- utils/generate-docs/Schema/SchemaNode/SchemaNode.test.ts
+++ utils/generate-docs/Schema/SchemaNode/SchemaNode.test.ts
@@ -41,6 +41,33 @@ describe("SchemaNode", () => {
     });
   });
 
+  describe("#sourcePathMkDocs", () => {
+    it("returns the location of the file relative to the schema when using pluggable url for root", () => {
+      const schema = new Schema(
+        [FIXTURE],
+        [],
+        [],
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      );
+      const schemaNode = new DummyNode(
+        schema,
+        FIXTURE,
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      );
+
+      const actual = schemaNode.jsonSourcePath();
+      expect(actual).toEqual(
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF/schema/files/TestFile.schema.json"
+      );
+    });
+  });
+
   describe("#outputPath", () => {
     it("returns the location of the markdown doc", () => {
       const schema = new Schema(
@@ -63,6 +90,33 @@ describe("SchemaNode", () => {
 
       const actual = schemaNode.docMdLink();
       expect(actual).toEqual("/docs/schema/files/TestFile.md");
+    });
+  });
+
+  describe("#outputPathMkDocs", () => {
+    it("returns the location of the markdown doc when using pluggable url for root", () => {
+      const schema = new Schema(
+        [FIXTURE],
+        [],
+        [],
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      );
+      const schemaNode = new DummyNode(
+        schema,
+        FIXTURE,
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      );
+
+      const actual = schemaNode.docMdLink();
+      expect(actual).toEqual(
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs/schema/files/TestFile"
+      );
     });
   });
 
