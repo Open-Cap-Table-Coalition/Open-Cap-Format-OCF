dc8f4cef9134cbdaefc05c237e98a68799e351b3
John Scrudato <jscrudato@gunder.com>
2022-11-22 11:50:52 -0500

    Update Schema tests for MkDocs.

--- utils/generate-docs/Schema/Schema.test.ts
+++ utils/generate-docs/Schema/Schema.test.ts
@@ -39,6 +39,29 @@ describe("Schema", () => {
     });
   });
 
+  // Should be no change for MkDocs, but let's test that's the case
+  describe("#findSchemaNodeByIdMkDocs", () => {
+    it("return the SchemaNode matching the given `id` in MkDocs", () => {
+      const schema = new Schema(
+        [FILE_SCHEMA_JSON_FIXTURE],
+        [],
+        [],
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
+      );
+
+      const actual = schema.findSchemaNodeById(
+        "https://opencaptablecoalition.com/schema/files/test_file"
+      );
+
+      expect(actual?.id()).toEqual(
+        "https://opencaptablecoalition.com/schema/files/test_file"
+      );
+    });
+  });
+
   describe("#filterSchemaNodesByParentType", () => {
     it("return the SchemaNode matching the given `id`", () => {
       const schema = new Schema(
@@ -49,6 +72,25 @@ describe("Schema", () => {
         "/docs",
         "",
         true
+      );
+
+      const actual = schema.filterSchemaNodesByParentType("files");
+
+      expect(actual).toHaveLength(1);
+    });
+  });
+
+  // Shoud be no change in this output when using pluggable urls
+  describe("#filterSchemaNodesByParentTypeMkDocs", () => {
+    it("return the SchemaNode matching the given `id`", () => {
+      const schema = new Schema(
+        [FILE_SCHEMA_JSON_FIXTURE],
+        [],
+        [],
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/README",
+        "https://open-cap-table-coalition.github.io/Open-Cap-Format-OCF/docs",
+        "https://github.com/Open-Cap-Table-Coalition/Open-Cap-Format-OCF",
+        false
       );
 
       const actual = schema.filterSchemaNodesByParentType("files");
